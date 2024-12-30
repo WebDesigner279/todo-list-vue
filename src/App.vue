ac7f6ef0bab432a90b1ece23c7f28176c2c37a25
valter <valter@teste.com>
2024-12-30 18:24:22 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!--Estrutura de Script-->
+<script setup>
+  const nomes = ['Gian', 'Paulo', 'Luiza', 'Mônica'];
+</script>
+
+<!--Estrutura da página-->
+<template>
+ <ul>
+  <li v-for="nome in nomes">
+    {{ nome }}
+  </li>
+ </ul>
+</template>
+
+<!--Estilo da página-->
+<style scoped>
+
+</style>
